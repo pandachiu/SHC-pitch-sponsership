c3353ead53e7728500bb9887948d9354ad7b1a41
Peter Chiu <peter@easyfundraising.org.uk>
2017-04-21 12:53:47 +0100

    Pitch square concept and styling. Main database seeder now calls the propertyDatabaseSeeder

--- resources/views/sponsor.blade.php
+++ resources/views/sponsor.blade.php
@@ -2,5 +2,17 @@
 
 @section('content')
 
+
+<div class="pitch">
+
+    @for ($i = 0; $i < 10; $i++)
+    <div class="pitch_row">
+        @for ($j = 0; $j < 8; $j++)
+        <div class="pitch__property"></div>
+        @endfor
+    </div>
+    @endfor
+
+</div>
 @endsection
 
