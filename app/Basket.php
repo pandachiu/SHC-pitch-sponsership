f9447ffba084ba18f9742b36cd6e7e14f8fb1b16
Peter Chiu <peter@easyfundraising.org.uk>
2017-04-23 17:35:16 +0100

    Adding relationships in basket and basketItem models

--- app/Basket.php
+++ app/Basket.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Basket extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function basketItem()
+    {
+        return $this->hasMany('App\BasketItem');
+    }
+
 }
--- app/BasketItem.php
+++ app/BasketItem.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class BasketItem extends Model
 {
-    //
+    public function basket()
+    {
+        return $this->belongsTo('App\Basket');
+    }
+
+    public function property()
+    {
+        return $this->belongsTo('App\Property');
+    }
 }
