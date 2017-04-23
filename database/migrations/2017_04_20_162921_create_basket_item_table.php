12ac1b0a1ab7b2db1fb75ef01b8e43d2605a0c63
Peter Chiu <peter@easyfundraising.org.uk>
2017-04-23 17:24:51 +0100

    fixing migration to change column name from basket_item to user_id

--- database/migrations/2017_04_20_162921_create_basket_item_table.php
+++ database/migrations/2017_04_20_162921_create_basket_item_table.php
@@ -14,7 +14,7 @@ class CreateBasketItemTable extends Migration
     {
         Schema::create('basket_items', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('basket_item');
+            $table->integer('user_id');
             $table->integer('propertyId');
             $table->timestamps();
         });
