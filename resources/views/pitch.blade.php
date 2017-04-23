0fd3101bcb5062f3d52a0ebd55de231d6d5cc582
Peter Chiu <peter@easyfundraising.org.uk>
2017-04-23 21:08:10 +0100

    adding pitch image and adjusting styling to account for image

--- resources/views/pitch.blade.php
+++ resources/views/pitch.blade.php
@@ -1,47 +1,49 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="pitch">
-    <div class="row">
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
+
+<div class="pitch" style="background-image:url('/assets/rsz_tmp786975523094921217.jpg')">
+    <div class="pitch_row">
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
     </div>
-    <div class="row">
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
+    <div class="pitch_row">
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
     </div>
-    <div class="row">
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
+    <div class="pitch_row">
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
     </div>
-    <div class="row">
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
-        <div class="col-xs-1 pitch__property"></div>
+    <div class="pitch_row">
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
+        <div class="pitch__property"></div>
     </div>
+    <div class="clearfix"></div>
 </div>
 @endsection
 
